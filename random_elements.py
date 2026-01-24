8c4f709e8f0139a4216ba555c28effa3a8687f01
Koyyalamudi Yaswanth <kammabbay2002@gmail.com>
2026-01-24 13:53:20 +0530

    Add files via upload

---
+++ random_elements.py
@@ -0,0 +1,21 @@
+import random as rand
+
+my_list = ['apple', 'banana', 'cherry', 'mango']
+random_item = rand.choice(my_list)
+print(random_item)
+
+print(rand.randint(1,100))
+li=[rand.randint(1,100) for _ in range(10)]
+print(li)
+
+print(rand.random())#gives output b/w 0,1 float
+
+print(rand.uniform(1, 10))#float range gives float value
+
+print(rand.sample(my_list, 2))#no duplicates in selection
+
+print(rand.choices(my_list, k=5))#duplication allowed
+
+print(rand.shuffle(li))
+
+print(rand.seed(10))
